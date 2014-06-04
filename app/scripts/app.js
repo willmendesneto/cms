fc93cd2a5400294b23cb122ad148875e95653d30
Ash-girl Chapfuwa <ashgirlchapfuwa@gmail.com>
2014-06-04 18:14:42 -0300

    add app.js from old cms

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,49 +1,29 @@
-var cms = angular.module('cmsApp', ['ngRoute']);
+'use strict';
 
-cms.config(function($routeProvider, $locationProvider) {
+var app = angular.module('cmsApp', ['ngRoute']);
+
+app.config(function ($routeProvider) {
   $routeProvider
-    .when('/auth', { controller: 'AuthenticationCtrl', templateUrl: 'views/auth.html' })
-    .when('/posts', { controller: 'PostsCtrl', templateUrl: 'views/posts.html' })
-    .when('/posts/:sha', { controller: 'PostCtrl', templateUrl: 'views/post.html' })
-    .otherwise({ redirectTo: '/auth' });
+    .when('/auth', {
+      templateUrl: 'views/auth.html',
+      controller: 'AuthCtrl'
+    })
+    .when('/posts', {
+      templateUrl: 'views/posts.html',
+      controller: 'PostsCtrl'
+    })
+    .when('/posts/:sha', {
+      templateUrl: 'views/post.html',
+      controller: 'PostCtrl'
+    })
+    .otherwise({
+      redirectTo: '/auth'
+    });
 });
 
-cms.factory('oauth', function () {
-  OAuth.initialize('S2shWzj2Cp87Mg4estazc6DFGQc');
-  return OAuth;
+app.config(function ($httpProvider) {
+  $httpProvider.defaults.useXDomain = true;
+  delete $httpProvider.defaults.headers.common['X-Requested-With'];
 });
 
-function postSearchFilter(posts, term) {
-  if (!term) return posts;
 
-  return _.filter(posts, function (post) {
-    return post.name.indexOf(term) != -1;
-  });
-}
-
-cms.filter('postSearchFilter', function() {
-  return postSearchFilter;
-});
-
-cms.directive('ckEditor', function() {
-  return {
-    require : '?ngModel',
-    link : function($scope, elm, attr, ngModel) {
-      var ck = CKEDITOR.replace(elm[0]);
-
-      ck.on('instanceReady', function() {
-        ck.setData(ngModel.$viewValue);
-      });
-
-      ck.on('pasteState', function() {
-        $scope.$apply(function() {
-          ngModel.$setViewValue(ck.getData());
-        });
-      });
-
-      ngModel.$render = function(value) {
-        ck.setData(ngModel.$modelValue);
-      };
-    }
-  };
-});
