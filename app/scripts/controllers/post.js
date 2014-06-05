7c0211346cb89554571ae9fbee28f7f07be9ec5c
Joao Paulo Leonidas Fernandes Dias da Silva and Rodrigo Alencar <rodrigovieira18+joao+rodrigo@gmail.com>
2014-06-05 18:40:39 -0300

    #12 fix jshintrc issues

--- app/scripts/controllers/post.js
+++ app/scripts/controllers/post.js
@@ -2,100 +2,99 @@
 'use strict';
 
 angular.module('cmsApp')
-  .controller('PostCtrl', function ($scope, $rootScope, $routeParams) {
+.controller('PostCtrl', function ($scope, $rootScope, $routeParams) {
 
-    function findPost(sha) {
-      return $rootScope.posts.filter(function(post) {
-        return post.sha === sha;
-      }).shift(0);
-    }
+  function findPost(sha) {
+    return $rootScope.posts.filter(function(post) {
+      return post.sha === sha;
+    }).shift(0);
+  }
 
-    function contentPath(sha) {
-      return 'https://api.github.com/repos/movimento-sem-terra/site-novo/git/blobs/'+sha;
-    }
+  function contentPath(sha) {
+    return 'https://api.github.com/repos/movimento-sem-terra/site-novo/git/blobs/'+sha;
+  }
 
-    var sha = $routeParams.sha;
-    $scope.post = findPost(sha);
+  var sha = $routeParams.sha;
+  $scope.post = findPost(sha);
 
-    $scope.menuTagOptions = [
-      'agricultura camponesa',
-      'agronegócio',
-      'direitos humanos',
-      'educação, cultura e comunicação',
-      'lutas e mobilizações',
-      'solidariedade internacional',
-      'meio ambiente',
-      'projeto popular',
-      'reforma agrária',
-      'transgênicos'
-    ];
+  $scope.menuTagOptions = [
+    'agricultura camponesa',
+    'agronegócio',
+    'direitos humanos',
+    'educação, cultura e comunicação',
+    'lutas e mobilizações',
+    'solidariedade internacional',
+    'meio ambiente',
+    'projeto popular',
+    'reforma agrária',
+    'transgênicos'
+  ];
 
-    $scope.sectionOptions = [
-      {label: 'Destaque', value: 'featured-news'},
-      {label: 'Debate', value: 'debate'},
-      {label: 'Capa', value: 'cover'},
-      {label: 'MST TV', value: 'tv'}
-    ];
+  $scope.sectionOptions = [
+    {label: 'Destaque', value: 'featured-news'},
+    {label: 'Debate', value: 'debate'},
+    {label: 'Capa', value: 'cover'},
+    {label: 'MST TV', value: 'tv'}
+  ];
 
-    $scope.labelOptions = [
-      {label: 'Artigo', value: 'articles'},
-      {label: 'Entrevista', value: 'interviews'},
-      {label: 'Reportagens Especiais', value: 'special-stories'}
-    ];
+  $scope.labelOptions = [
+    {label: 'Artigo', value: 'articles'},
+    {label: 'Entrevista', value: 'interviews'},
+    {label: 'Reportagens Especiais', value: 'special-stories'}
+  ];
 
-    $scope.menuTag = undefined;
-    $scope.section = undefined;
-    $scope.label = undefined;
-    $scope.tag = '';
-	$scope.images_hd = undefined;
+  $scope.menuTag = undefined;
+  $scope.section = undefined;
+  $scope.label = undefined;
+  $scope.tag = '';
+  $scope.imagesHD = undefined;
 
-    $scope.$watch('label', function (newval) {
-      $scope.post.setLabel(newval);
-    });
-
-    $scope.$watch('menuTag', function (newval) {
-      $scope.post.setMenuItem(newval);
-    });
-
-    $scope.$watch('section', function (newval) {
-      $scope.post.setSection(newval);
-    });
-
-    $rootScope.github.get(contentPath(sha)).done(function(data) {
-      $scope.post.loadContentFromJekyllData(atob(data.content));
-      $scope.$apply();
-    });
-
-	$scope.save = function(post) {
-		$scope.post.setImagesHD($scope.uploadImage());
-		$rootScope.github.put(filePath(post.name), {
-			data: JSON.stringify(post.commitData())
-		}).done(function(data) {
-			alert('Post salvo com sucesso!');
-		}).fail(function(data) {
-			console.log('error data:', data);
-		});
-	};
-
-	$scope.uploadImage = function() {
-		var xhr = new XMLHttpRequest();
-		var form = new FormData();
-		var that = this;
-	
-		var postedFiles = document.getElementById('imgFile');
-		if (postedFiles.files.length > 0) {
-			var imgFile = postedFiles.files[0];
-			form.append('myfile', imgFile);
-			form.append('token',$rootScope.github.access_token);
-			xhr.open('POST', 'http://mst-image-service.herokuapp.com/upload', false);      
-			xhr.send(form);
-			return xhr.response;
-		} else {
-			return '';
-		}
-	};
-	
-    function filePath(name) {
-      return '/repos/movimento-sem-terra/site-novo/contents/_drafts/'+name;
-    }
+  $scope.$watch('label', function (newval) {
+    $scope.post.setLabel(newval);
   });
+
+  $scope.$watch('menuTag', function (newval) {
+    $scope.post.setMenuItem(newval);
+  });
+
+  $scope.$watch('section', function (newval) {
+    $scope.post.setSection(newval);
+  });
+
+  $rootScope.github.get(contentPath(sha)).done(function(data) {
+    $scope.post.loadContentFromJekyllData(atob(data.content));
+    $scope.$apply();
+  });
+
+  $scope.save = function(post) {
+    $scope.post.setImagesHD($scope.uploadImage());
+    $rootScope.github.put(filePath(post.name), {
+      data: JSON.stringify(post.commitData())
+    }).done(function() {
+      alert('Post salvo com sucesso!');
+    }).fail(function(data) {
+      console.log('error data:', data);
+    });
+  };
+
+  $scope.uploadImage = function() {
+    var xhr = new XMLHttpRequest();
+    var form = new FormData();
+
+    var postedFiles = document.getElementById('imgFile');
+    if (postedFiles.files.length > 0) {
+      var imgFile = postedFiles.files[0];
+      form.append('myfile', imgFile);
+      form.append('token',$rootScope.github.accessToken);
+      xhr.open('POST', 'http://mst-image-service.herokuapp.com/upload', false);
+      xhr.send(form);
+      return xhr.response;
+    } else {
+      return '';
+    }
+  };
+
+  function filePath(name) {
+    return '/repos/movimento-sem-terra/site-novo/contents/_drafts/'+name;
+  }
+});
--- app/scripts/services/post.js
+++ app/scripts/services/post.js
@@ -3,101 +3,101 @@
 
 angular.module('cmsApp').service('Post', function Post(_, jsyaml) {
 
-    return {
-      makePost: function(data) {
-        var self = {};
-        _.extend(self, data);
+  return {
+    makePost: function(data) {
+      var self = {};
+      _.extend(self, data);
 
-        self.loadContentFromJekyllData = function (data) {
-          var parts = decodeURIComponent(escape(data)).split('---');
-          self.content = {
-            text: parts.pop().replace(/^\n/, ''),
-            meta: jsyaml.load(parts.pop())
-          };
+      self.loadContentFromJekyllData = function (data) {
+        var parts = decodeURIComponent(escape(data)).split('---');
+        self.content = {
+          text: parts.pop().replace(/^\n/, ''),
+          meta: jsyaml.load(parts.pop())
         };
+      };
 
-        self.convertContentToJekyllData = function () {
-          if (!self.content) {
-            return '';
-          }
+      self.convertContentToJekyllData = function () {
+        if (!self.content) {
+          return '';
+        }
 
-          var compiled = ['---', jsyaml.dump(self.content.meta), '---', self.content.text].join('\n');
-          return unescape(encodeURIComponent(compiled));
+        var compiled = ['---', jsyaml.dump(self.content.meta), '---', self.content.text].join('\n');
+        return unescape(encodeURIComponent(compiled));
+      };
+
+      self.commitData = function() {
+        return {
+          sha: self.sha,
+          content: btoa(self.convertContentToJekyllData()),
+          message: 'commit from cms'
         };
+      };
 
-        self.commitData = function() {
-          return {
-            sha: self.sha,
-            content: btoa(self.convertContentToJekyllData()),
-            message: 'commit from cms'
-          };
-        };
+      self.setMenuItem = function(menuItemName) {
+        if (!self.content) {
+          return;
+        }
 
-        self.setMenuItem = function(menuItemName) {
-          if (!self.content) {
-            return;
-          }
+        self.content.meta.tags = _.filter(self.content.meta.tags, function (el) {
+          return !(/^menu:/).test(el);
+        });
 
-          self.content.meta.tags = _.filter(self.content.meta.tags, function (el) {
-            return !(/^menu:/).test(el);
-          });
+        if (!!menuItemName){
+          self.content.meta.tags.push('menu:'+menuItemName);
+        }
+      };
 
-          if (!!menuItemName){
-            self.content.meta.tags.push('menu:'+menuItemName);
-          }
-        };
+      self.setLabel = function(label) {
+        if (!self.content) {
+          return;
+        }
 
-        self.setLabel = function(label) {
-          if (!self.content) {
-            return;
-          }
+        if (!label) {
+          self.content.meta.label = '';
+          return;
+        }
 
-          if (!label) {
-            self.content.meta.label = '';
-            return;
-          }
+        self.content.meta.label = label.value;
+      };
 
-          self.content.meta.label = label.value;
-        };
+      self.setSection = function(section) {
+        if (!self.content) {
+          return;
+        }
 
-        self.setSection = function(section) {
-          if (!self.content) {
-            return;
-          }
+        if (!section) {
+          self.content.meta.section = '';
+          return;
+        }
 
-          if (!section) {
-            self.content.meta.section = '';
-            return;
-          }
+        self.content.meta.section = section.value;
+      };
 
-          self.content.meta.section = section.value;
-        };
+      self.setImagesHD = function(imagesHD) {
+        if (!self.content) {
+          return;
+        }
 
-		self.setImagesHD = function(images_hd) {
-			if (!self.content) {
-				return;
-			}
-		
-			if (!images_hd) {
-				self.content.meta.images_hd = '';
-				return;
-			}
-				  
-			self.content.meta.images_hd = images_hd;
-		};
-		
-        self.addNewTag = function(customTag) {
-          if (!self.content.meta.tags) {
-            return;
-          }
+        if (!imagesHD) {
+          self.content.meta.imagesHD = '';
+          return;
+        }
 
-          customTag = 'personalizada:'+customTag;
-          self.content.meta.tags.push(customTag);
-        };
-		
-		return self;
-      }
-    };
+        self.content.meta.imagesHD = imagesHD;
+      };
 
-    // AngularJS will instantiate a singleton by calling "new" on this function
-  });
+      self.addNewTag = function(customTag) {
+        if (!self.content.meta.tags) {
+          return;
+        }
+
+        customTag = 'personalizada:'+customTag;
+        self.content.meta.tags.push(customTag);
+      };
+
+      return self;
+    }
+  };
+
+  // AngularJS will instantiate a singleton by calling "new" on this function
+});
