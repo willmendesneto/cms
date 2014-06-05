fd5a6cea13d1e52785e8942c20da2a9cdfc9e41e
Ash-girl Chapfuwa <ashgirlchapfuwa@gmail.com>
2014-06-05 17:18:48 -0300

    #24 fix jshintrc issues

--- app/scripts/controllers/authentication.js
+++ app/scripts/controllers/authentication.js
@@ -1,11 +1,15 @@
-var cms = angular.module('cmsApp')
-    .controller('AuthenticationCtrl', function AuthenticationCtrl($scope, $rootScope, $location, oauth) {
-      $scope.authenticate = function() {
-        oauth.popup('github', function(err, res) {
-          if(err) return alert(err);
-          $rootScope.github = res;
-          $location.path('/posts');
-          $scope.$apply();
-        });
-      };
-    });
+'use strict';
+
+angular.module('cmsApp')
+      .controller('AuthenticationCtrl', function AuthenticationCtrl($scope, $rootScope, $location, oauth) {
+        $scope.authenticate = function() {
+          oauth.popup('github', function(err, res) {
+            if(err){
+              return window.alert(err);
+            }
+            $rootScope.github = res;
+            $location.path('/posts');
+            $scope.$apply();
+          });
+        };
+      });
