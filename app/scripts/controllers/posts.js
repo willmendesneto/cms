f57afa4b347ddb58e6a9ba711e1132c05bd50d5b
Ash-girl Chapfuwa <ashgirlchapfuwa@gmail.com>
2014-06-18 18:39:06 -0300

    +@frnlins - #54 - add  to scope

--- app/scripts/controllers/posts.js
+++ app/scripts/controllers/posts.js
@@ -3,33 +3,30 @@
 angular.module('cmsApp')
   .controller('PostsCtrl', function ($scope, $rootScope, _, Post, $filter, Posts) {
 
-  $rootScope.posts = [];
   Posts.success(function(data){
-    $rootScope.posts = _.map(data, Post.makePost);
+    $scope.$apply(function() {
+      $rootScope.posts = _.map(data, Post.makePost);
+
+      $scope.currentPage = 0;
+
+      $scope.pageSize = 10;
+
+      $scope.numberOfPages = function(){
+        return Math.ceil($rootScope.posts.length/$scope.pageSize);
+      };
+
+      $scope.tableOrderBy = function(expression, reverse){
+        $scope.posts = $filter('orderBy')($rootScope.posts, expression, reverse);
+      };
+
+      $scope.nextPage = function(){
+        $scope.currentPage=$scope.currentPage+1;
+      };
+
+      $scope.previousPage = function(){
+        $scope.currentPage=$scope.currentPage-1;
+      };
+    });
   });
-
-  $scope.currentPage = 0;
-
-  $scope.pageSize = 10;
-
-  /**
-   * Returns the number page based in params
-   * @return {[type]} [description]
-   */
-  $scope.numberOfPages = function(){
-    return Math.ceil($rootScope.posts.length/$scope.pageSize);
-  };
-
-  $scope.tableOrderBy = function(expression, reverse){
-    $scope.posts = $filter('orderBy')($rootScope.posts, expression, reverse);
-  };
-
-  $scope.nextPage = function(){
-    $scope.currentPage=$scope.currentPage+1;
-  };
-
-  $scope.previousPage = function(){
-    $scope.currentPage=$scope.currentPage-1;
-  };
 
 });
