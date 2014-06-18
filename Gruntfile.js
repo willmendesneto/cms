0484692e02bd42ffd88789c095358332ec128047
Joao Paulo Leonidas Fernandes Dias da Silva and Rodrigo Alencar <rodrigovieira18+joao+rodrigo@gmail.com>
2014-06-18 19:02:27 -0300

    Fixing slash in the end

--- Gruntfile.js
+++ Gruntfile.js
@@ -304,7 +304,7 @@ module.exports = function (grunt) {
         constants: {
           ENV: {
             name: 'test',
-            repository: 'movimento-sem-terra/staging'
+            repository: 'movimento-sem-terra/staging/'
           }
         }
       },
@@ -315,7 +315,7 @@ module.exports = function (grunt) {
         constants: {
           ENV: {
             name: 'development',
-            repository: 'movimento-sem-terra/staging'
+            repository: 'movimento-sem-terra/staging/'
           }
         }
       },
