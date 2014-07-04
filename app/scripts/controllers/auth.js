bd8f8462b38f19422b6a77c03e70f00d59a75d41
jpaulorio <jpaulorio@outlook.com>
2014-07-04 09:00:36 -0300

    Fix #78 - Only allows members of Journalist team to access the application

--- app/scripts/controllers/auth.js
+++ app/scripts/controllers/auth.js
@@ -2,30 +2,41 @@
 'use strict';
 
 angular.module('cmsApp')
-  .controller('AuthCtrl', function ($scope, $rootScope, $location, oauth, GithubClient) {
+  .controller('AuthCtrl', function ($scope, $rootScope, $location, oauth, GithubClient, $q) {
     $scope.authenticate = function() {
       oauth.popup('github', function(err, res) {
         if(err) {
           return alert(err);
         }
-        $rootScope.github = res;
-        var isJournalist = false;
+        $rootScope.github = res;        
         
-        res.get('user/teams').success(function(data){
-          for (var i=0; i<data.length; i++) {
-            var team = data[i].name;
-            if (team == 'Jornalistas') {
-              isJournalist = true;
-              break;             
+        var authenticate = function() {
+          var deferred = $q.defer();
+          res.get('user/teams').success(function(data){            
+            var isJournalist = false;
+
+            for (var i=0; i<data.length; i++) {
+              var team = data[i].name;
+              
+              if (team == 'Jornalistas') {              
+                isJournalist = true;                
+                break;             
+              }
             }
-          }
-        }).
-        done(function(){
+            deferred.resolve(isJournalist);
+          });
+          return deferred.promise;
+        };
+        
+        var promise = authenticate();
+        promise.then(function(isJournalist) {
           if (isJournalist) {
             $scope.$apply();
             $location.path('/posts');
+          } else {
+            alert('Acesso negado!');
           }
-        });        
+        });                
       });
     };
   });
