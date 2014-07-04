ce997f331aa2be119593e375beb00dda8d3671ef
jpaulorio <jpaulorio@outlook.com>
2014-07-04 10:49:40 -0300

    Fix #79 - Add logged user name to site header

--- app/scripts/controllers/auth.js
+++ app/scripts/controllers/auth.js
@@ -2,14 +2,22 @@
 'use strict';
 
 angular.module('cmsApp')
-  .controller('AuthCtrl', function ($scope, $rootScope, $location, oauth, GithubClient, $q) {
+  .controller('AuthCtrl', function ($scope, $rootScope, $location, oauth, $q, $timeout) {
     $scope.authenticate = function() {
       oauth.popup('github', function(err, res) {
         if(err) {
           return alert(err);
         }
         $rootScope.github = res;        
-        
+        var userInfo = function() {
+          var deferred = $q.defer();
+          res.get('user').success(function(data){            
+            var userName = data.name;            
+            deferred.resolve(userName);
+          });
+          return deferred.promise;
+        };
+
         var authenticate = function() {
           var deferred = $q.defer();
           res.get('user/teams').success(function(data){            
@@ -28,15 +36,19 @@ angular.module('cmsApp')
           return deferred.promise;
         };
         
-        var promise = authenticate();
-        promise.then(function(isJournalist) {
+        var userPromise = userInfo();
+        userPromise.then(function(userName) {                              
+          $rootScope.loggedUser = userName;
+        });
+
+        var authPromise = authenticate();
+        authPromise.then(function(isJournalist) {
           if (isJournalist) {
-            $scope.$apply();
             $location.path('/posts');
           } else {
             alert('Acesso negado!');
           }
-        });                
+        });
       });
     };
   });
