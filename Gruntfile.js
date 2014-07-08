5ec10bc9c418ef79bf031d5153e9bec637731bac
RatoX <rodrigovieira18@gmail.com>
2014-07-08 11:38:05 -0300

    Removing cdnify task

--- Gruntfile.js
+++ Gruntfile.js
@@ -206,13 +206,6 @@ module.exports = function (grunt) {
       }
     },
 
-    // Replace Google CDN references
-    cdnify: {
-      dist: {
-        html: ['<%= yeoman.dist %>/*.html']
-      }
-    },
-
     // Copies remaining files to places other tasks can use
     copy: {
       dist: {
@@ -386,7 +379,6 @@ module.exports = function (grunt) {
     'concat',
     'ngmin',
     'copy:dist',
-    'cdnify',
     'cssmin',
     'uglify',
     'rev',
