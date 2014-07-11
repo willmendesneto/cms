6eff7fb077443ce8af1c91fbc82b1451cb1e4326
Ash-girl Chapfuwa <ashgirlchapfuwa@gmail.com>
2014-07-10 23:42:37 -0300

    #87 rename util to dateutil

---
+++ app/scripts/services/dateutil.js
@@ -0,0 +1,20 @@
+'use strict';
+
+angular.module('cmsApp')
+  .factory('DateUtil', function () {
+    return {
+      getTime: function(){
+          return new Date();
+        },
+
+      getTimestamp: function(date, time) {
+        var year = date.getFullYear();
+        var month = date.getMonth();
+        var day = date.getDate();
+        var hours = time.getHours();
+        var minutes = time.getMinutes();
+        return new Date(year + '/' + month + '/' + day + ' ' + hours + ':' + minutes).getTime();
+      }
+    };
+
+  });
