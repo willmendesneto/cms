37d31827bdf62c29356ffc40e7bd5971e289b4e0
jpaulorio <jpaulorio@outlook.com>
2014-07-18 14:00:28 -0300

    Fix missing require

--- web.js
+++ web.js
@@ -1,4 +1,5 @@
 var express           = require('express'),
+	path              = require('path'),
     app               = express(),
     server            = require('http').createServer(app)
     ;
