c942ee1fe385fbe85df4aed1abe0bb665f3478e3
jpaulorio <jpaulorio@outlook.com>
2014-07-18 13:30:16 -0300

    Fix node server

--- web.js
+++ web.js
@@ -1,5 +1,20 @@
- app.set('port', 80);
+var express           = require('express'),
+    app               = express(),
+    server            = require('http').createServer(app)
+    ;
 
- server.listen(app.get('port'), function () {
-   console.log('Express server listening on port ' + server.address().port);
- });
+startServer();
+
+function startServer () {
+  app.set('port', 80);
+
+  server.listen(app.get('port'), function () {
+    console.log('Express server listening on port ' + server.address().port);
+  });
+}
+
+function setupMiddlewares () {
+  app.use(express.static(path.join(__dirname, 'app')));
+}
+
+module.exports = app;
