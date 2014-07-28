9c62fa035e948270d7cf77a24cae9671c76d11af
RatoX <rodrigovieira18@gmail.com>
2014-07-27 22:29:51 -0300

    Fix search

--- app/scripts/controllers/posts.js
+++ app/scripts/controllers/posts.js
@@ -1,59 +1,37 @@
 'use strict';
 
 angular.module('cmsApp')
-  .controller('PostsCtrl', function ($scope, $rootScope, _, $filter, GitRepository, $timeout) {
+  .controller('PostsCtrl', function ($scope, $rootScope, _, $filter, GitRepository, $timeout, Alert) {
 
-    $scope.year = 2014;
-    $scope.month = 6;
-
-    function filterPostByDate(data){
-      var date;
-      data.filter(function(item){
-        date = item.name.substring(0, 10).split('-');
-        item.reverseDate = date.join('');
-        item.publishDate = date.reverse().join('/');
-      });
-      return data;
-    }
+    $scope.year = (new Date()).getFullYear();
+    $scope.month = (new Date()).getMonth();
+    $scope.searchDone = true;
 
     $scope.find = function(){
-      GitRepository.getPosts($scope.year, $scope.month).success(function(data){
+      $scope.searchDone = false;
+      $scope.posts = [];
+      GitRepository.getPosts($scope.year, $scope.month)
+      .success(function(data){
         $timeout(function(){
-
-          data = filterPostByDate(data);
-
           $scope.posts = _.map(data, function(data){
             return {
               sha: data.sha,
-              publishDate: data.publishDate,
               name: data.name,
               path: data.path,
               year: $scope.year,
               month: $scope.month,
-              reverseDate: data.reverseDate
             };
           });
 
-          $scope.reverse = true;
-
-          $scope.predicate = 'reverseDate';
-
-          $scope.currentPage = 0;
-
-          $scope.pageSize = 10;
-
-          $scope.numberOfPages = function(){
-            return Math.ceil($scope.filteredData.length/$scope.pageSize);
-          };
-
-          $scope.nextPage = function(){
-            $scope.currentPage=$scope.currentPage+1;
-          };
-
-          $scope.previousPage = function(){
-            $scope.currentPage=$scope.currentPage-1;
-          };
         }, 0);
+      }).error(function(error){
+        if(error.status == 404){
+          Alert.showInfo('Vixe, a pesquisa não encontrou nada, tente com outras opções.');
+        }else{
+          Alert.showError(error.status, error.responseText);
+        }
+      }).complete(function(){
+        $scope.searchDone = true;
       });
     };
   });
