c2f08285cd1de8c2deb74e99b549f0b65e851631
RatoX <rodrigovieira18+raphael+rodrigo@gmail.com>
2014-08-08 18:31:38 -0300

    Refactory gitrepostory again

---
+++ app/scripts/services/collections/post.js
@@ -0,0 +1,40 @@
+'use strict';
+
+angular.module('cmsApp')
+  .factory('PostCollection',['$rootScope','DateUtil','ENV', function($rootScope, DateUtil, ENV) {
+
+    function getYearMonthFolder() {
+      return DateUtil.applyFormat('yyyy/MM/');
+    }
+
+    function githubGet(year, month, filename) {
+      var yearMonth = DateUtil.applyFormat('yyyy/MM', new Date(year,month));
+      var url = getRepositoryAddress(yearMonth+filename);
+      return $rootScope.github.get(url ,{
+        cache: false
+      });
+    }
+
+    function getRepositoryAddress (filename) {
+      return '/repos/'+ ENV.repository + 'contents/_posts/' +filename;
+    }
+
+    var factory = {
+      find: function(year, month){
+        return githubGet(year, month,'');
+      },
+      get: function(year, month, filename){
+        return githubGet(year, month,'/'+filename);
+      },
+      save: function(filename, data){
+        var yearMonthFolder = getYearMonthFolder();
+        var url = getRepositoryAddress(yearMonthFolder+filename);
+        return $rootScope.github.put(url, {
+          data: data,
+          cache: false
+        });
+      }
+    };
+
+    return factory;
+  }]);
--- app/scripts/services/gitrepository.js
+++ app/scripts/services/gitrepository.js
@@ -1,44 +1,9 @@
 'use strict';
 
 angular.module('cmsApp')
-  .factory('GitRepository',['$rootScope', '$location', 'DateUtil', 'ENV', function($rootScope, $location, DateUtil, ENV){
-
-  function getYearMonthFolder() {
-    return DateUtil.applyFormat('yyyy/MM/');
-  }
-
-  function githubGet(year, month, filename) {
-    var yearMonth = DateUtil.applyFormat('yyyy/MM', new Date(year,month));
-    var url = getRepositoryAddress(yearMonth+filename);
-    return $rootScope.github.get(url ,{
-      cache: false
-    });
-  }
-
-  function getRepositoryAddress (filename) {
-    return '/repos/'+ ENV.repository + 'contents/_posts/' +filename;
-  }
-
-  var post = {
-    find: function(year, month){
-      return githubGet(year, month,'');
-    },
-    get: function(year, month, filename){
-      return githubGet(year, month,'/'+filename);
-    },
-    save: function(filename, data){
-      var yearMonthFolder = getYearMonthFolder();
-      var url = getRepositoryAddress(yearMonthFolder+filename);
-      return $rootScope.github.put(url, {
-        data: data,
-        cache: false
-      });
-    }
-  };
-
-
+  .factory('GitRepository',['$rootScope', 'PostCollection', function($rootScope, PostCollection){
   var GitRepository = {
-    post: post,
+    post: PostCollection,
 
     getUser: function() {
       return $rootScope.github.get('user');
