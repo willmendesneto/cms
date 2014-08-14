5d031a6e668360f108e4368f69ac1c6589d315c8
RatoX <rodrigovieira18+raphael+rodrigo@gmail.com>
2014-08-14 19:15:27 -0300

    Fixed problem to show icons and images.

--- Gruntfile.js
+++ Gruntfile.js
@@ -218,7 +218,7 @@ module.exports = function (grunt) {
             '*.{ico,png,txt}',
             '.htaccess',
             'bower_components/**/*',
-            'images/{,*/}*.{webp}',
+            'images/{,*/}*.{webp,png,jpg,gif}',
             'fonts/*',
             'ckeditor-plugins/**/*'
           ]
